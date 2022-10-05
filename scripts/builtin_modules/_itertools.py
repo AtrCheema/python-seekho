9b40275386353c0dac74f3eefdd35703078321ea
Ather Abbas <ather_abbas786@yahoo.com>
2022-10-05 14:07:07 +0900

    updated

---
+++ scripts/builtin_modules/_itertools.py
@@ -0,0 +1,53 @@
+"""
+====================
+itertools
+====================
+
+.. important::
+  This lesson is still under development.
+
+"""
+
+import itertools
+
+
+# %%
+# combinations
+# -------------
+x = [1,2,3]
+
+# %%
+print([x for x in itertools.combinations(x, 2)])
+
+# %%
+
+print([x for x in itertools.combinations(x, 3)])
+
+# %%
+
+print([x for x in itertools.combinations(x, 10)])
+
+# %%
+# product
+# --------
+
+for prod in (itertools.product([1, 2], [11, 12])):
+    print(prod)
+
+# %%
+
+for prod in (itertools.product([1,2,3], [11, 12, 13])):
+    print(prod)
+
+# %%
+for prod in (itertools.product([1, 2], [11, 12], [21, 23])):
+    print(prod)
+
+# %%
+# permutations
+# ------
+print([x for x in itertools.permutations([1,2,3])])
+
+# %%
+
+print([x for x in itertools.permutations([1,2,3], 2)])
