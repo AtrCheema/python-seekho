c2f78aafbdd1f0180c48e3cd45c0faa164a39f29
AtrCheema <ather_abbas786@yahoo.com>
2024-07-14 21:46:19 +0300

    updated

--- scripts/advanced/interfacing_with_cpp.py
+++ scripts/advanced/interfacing_with_cpp.py
@@ -6,8 +6,7 @@
 This lesson describes how to call functions/modules written in C++ into python and
 """
 
-
-
+import time   
 import ctypes
 import subprocess
 from ctypes import c_char_p, c_uint, c_size_t, POINTER, Structure
@@ -15,13 +14,28 @@ from ctypes import c_char_p, c_uint, c_size_t, POINTER, Structure
 import numpy as np
 import pandas as pd
 
+# %%
+
+start = time.time()
+for i in range(100):
+    pd.read_csv('daily_q.csv', index_col=0)
+
+print(f"with pandas: {time.time() - start:.2f} seconds")
+
+# %%
+
 # Specify the C++ source file and the output executable name
 cpp_source_file = 'read_csv_gpt.cpp'
 output_library = 'read_csv_gpt.so'
 
 # Compile the C++ code as a library
 compile_command = f'g++ -shared -o {output_library}  -fPIC {cpp_source_file}'
-subprocess.run(compile_command, shell=True)
+process = subprocess.run(compile_command, shell=True, text=True, capture_output=True)
+
+if process.returncode != 0:
+    print(process.stderr)
+else:
+    print('Compilation successful')
 
 # Define the CSVRow structure
 class CSVRow(Structure):
@@ -62,58 +76,13 @@ def read_csv(file_path):
 
 df = read_csv(b'daily_q.csv')
 
-import time
 start = time.time()
-for i in range(50):
+for i in range(100):
     read_csv(b'daily_q.csv')
 
-print(f"Time taken to read the CSV file 5 times: {time.time() - start:.2f} seconds")
-
-
-start = time.time()
-for i in range(50):
-    df1 = pd.read_csv('daily_q.csv', index_col=0)
-print(f"Time taken to read the CSV file 5 times: {time.time() - start:.2f} seconds")
-
+print(f"With C++: {time.time() - start:.2f} seconds")
 
 # %%
-
-import os
-import time
-import ctypes
-import pandas as pd
-import numpy as np
-import subprocess
-
-
-# Specify the C++ source file and the output executable name
-cpp_source_file = 'read_csv_gpt.cpp'
-output_library = 'read_csv_gpt.so'
-
-# Compile the C++ code as a library
-compile_command = f'g++ -shared -o {output_library}  -fPIC {cpp_source_file}'
-process = subprocess.run(compile_command, shell=True, text=True, capture_output=True)
-
-if process.returncode != 0:
-    print(process.stderr)
-else:
-    print('Compilation successful')
-
-# %%
-print(process.stdout)
-
-# %%
-
-# Define the CSVRow structure
-class CSVRow(ctypes.Structure):
-    _fields_ = [("date", ctypes.c_char * 20),
-                ("floatValue", ctypes.c_float)]
-
-# %%
-#print(os.getcwd())
-
-# %%
-#print(os.listdir(os.getcwd()))
 
 # Load the shared library
 csv_reader = ctypes.CDLL(f'./{output_library}')   # Use the correct path for your .so/.dll file
@@ -125,6 +94,7 @@ csv_reader.processCSVFileFast.restype = ctypes.c_void_p
 csv_reader.freeCSVRowsFast.argtypes = [ctypes.c_void_p]
 csv_reader.freeCSVRowsFast.restype = None
 
+# %%
 
 def read_csvFast(file_path):
     # Call the processCSVFileFast function
@@ -151,8 +121,39 @@ def read_csvFast(file_path):
     csv_reader.freeCSVRowsFast(ptr)
     return df
 
+# %%
+
 start = time.time()
-for i in range(50):
+for i in range(100):
     df = read_csvFast(b'daily_q.csv')
 
-print(f"Time taken to read the CSV file 5 times: {time.time() - start:.2f} seconds")
+print(f"With C++: {time.time() - start:.2f} seconds")
+
+# %%
+
+
+def read_csvFast1(file_path):
+    # Call the processCSVFileFast function
+    total = ctypes.c_uint(0)
+    size = ctypes.c_size_t(0)
+    row_size = ctypes.c_size_t(0)
+    ptr = csv_reader.processCSVFileFast(file_path, ctypes.byref(total), ctypes.byref(size), ctypes.byref(row_size))
+
+    # Convert the pointer to a numpy array
+    buffer = (ctypes.c_char * (size.value * row_size.value)).from_address(ptr)
+    data = np.frombuffer(buffer, dtype=[('date', 'S20'), (str(total.value), 'f4')])
+
+    # Convert the 'date' column to string
+    #index = df['date'].str.decode('utf-8')
+
+    # Free the allocated memory
+    csv_reader.freeCSVRowsFast(ptr)
+    return data
+
+# %%
+
+start = time.time()
+for i in range(100):
+    df = read_csvFast1(b'daily_q.csv')
+
+print(f"With C++ in numpy: {time.time() - start:.2f} seconds")
